f2f7c5c50ff6280ca4a42cda97cbb28177113e81
jwallace42 <47819219+jwallace42@users.noreply.github.com>
2019-07-09 20:04:14 -0400

    Update main.cpp

--- IMU/main.cpp
+++ IMU/main.cpp
@@ -1,63 +1,37 @@
 #include <Arduino.h>
-#include "imu.hpp"
+#include "InputDevice.hpp"
 
-/////////////////////////////////////////////////////////
-//GLOBAL VARIABLES 
-/////////////////////////////////////////////////////////
+
+int led = 13;
 elapsedMicros time;
-
-bool debug = true; 
 
 // Variables for debugging
 const int printTimer = 10000;
 int lastPrint = 0;
 
-// led 
-int led = 13; 
-
-// Filter sample rate
-const int updateTime = 4000;
-int lastUpdate = 0;
-
-// IMU variable outputs 
-extern float pitch, roll, yaw;
-extern float pitchD, rollD, yawD;
+extern float pitchDes, rollDes, yawDes;    //Desired Angles [deg]
+extern float pitchDesD, rollDesD, yawDesD; //Desired Rates [deg/s]
 
 void setup() {
+  // put your setup code here, to run once:
 
   // Connect to serial monitor 
   Serial.begin(115200);
   while(!Serial);
   Serial.println("DEBUGING");
 
-  // Intilizise the IMU 
-  ImuIntilization();
+  void InitializeInputDevice();
 
   // Setup completed 
-  digitalWrite(led,HIGH);
+  digitalWrite(led, HIGH);
   Serial.println("Finished Setup");
   delay(1000);
-
 }
 
-void loop() 
-{
-	// Update pulse to motors every 250hz
-	if ((time - lastUpdate) > updateTime)
-	{
-		lastUpdate = time;
-
-		// Get the rates and angles
-		GetIMU();
-	}
-
-	// Check output variables
-	if (debug == true)
-	{
-		if ((time - lastPrint) >= printTimer)
+void loop() {
+  if ((time - lastPrint) >= printTimer)
 		{
 			lastPrint = time;
-			Serial.println(yawD);
+			Serial.println(pitchDesD);
 		}
-	}
 }
